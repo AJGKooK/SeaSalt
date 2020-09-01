cb3eec02b369cea7b792cc7f6b112747adae69cf
Eddie Mahoney <ermahone@iastate.edu>
2020-09-01 15:17:46 -0500

    Web Socket Config

--- Experiments/Eddie/SpringBoot/complete/src/main/java/com/serviceapplication/WebSocketController.java
+++ Experiments/Eddie/SpringBoot/complete/src/main/java/com/serviceapplication/WebSocketController.java
@@ -5,13 +5,24 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.Random;
+
 @Controller
 public class WebSocketController {
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/chat")
+    private static final String[] compliments = {
+            "You look great today ",
+            "I hope you have a good day ",
+            "You're amazing ",
+            "I believe in you "
+    };
+
+    private final Random random = new Random();
+
+    @MessageMapping("/inbound")
+    @SendTo("/outbound")
     public WebSocketSend compliment(WebSocketReceiver message) throws Exception {
         Thread.sleep(1000);
-        return new WebSocketSend("You look great today " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        return new WebSocketSend(compliments[random.nextInt(compliments.length - 1)] + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }
