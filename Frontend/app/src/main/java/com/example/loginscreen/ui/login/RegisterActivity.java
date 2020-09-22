d6f0988f2c1fb054eef20262e8910f0eb499d0fe
cjurenic <cjurenic@iastate.edu>
2020-09-22 18:49:57 -0500

    started AsyncTask(minimal)

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/RegisterActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/RegisterActivity.java
@@ -2,52 +2,33 @@ package com.example.loginscreen.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.client.RestTemplate;
 import android.os.Bundle;
-import android.app.Activity;
 import android.os.AsyncTask;
-import android.os.StrictMode;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.StrictMode;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
-import com.example.loginscreen.R;
 
-import java.io.IOException;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.loginscreen.R;
 import java.util.HashMap;
 import java.util.Map;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
-    private String API_URL = "http://coms-309-ug-09.cs.iastate.edu";
+    private static String API_URL = "http://coms-309-ug-09.cs.iastate.edu/database/add/";
     private EditText username, password;
     private Button submit;
-    private OkHttpClient client;
     private Map<String, String> map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,57 +49,44 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    public boolean register() {
-        final String[] result = new String[1];
-        RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
-                .addFormDataPart("username", username.getText().toString().trim())
-                .addFormDataPart("password", password.getText().toString().trim()).build();
+    private void register() {
+        final String username = this.username.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
 
-        final Request request = new Request.Builder().url("http://coms-309-ug-09.cs.iastate.edu/database/add").post(requestBody).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                runOnUiThread(new Runnable() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
+                new Response.Listener<String>() {
                     @Override
-                    public void run() {
-                        result[0] = "fail";
-                    }
-                });
+                    public void onResponse(String response) {
+                        try {
 
-
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try{
-                            result[0] = response.body().string();
-                        } catch (IOException ioe){
-                            result[0] = "Error during get body";
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success == true){
+                                Toast.makeText(RegisterActivity.this, "Register Success", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(RegisterActivity.this, "Register Error!" + e.toString(), Toast.LENGTH_SHORT).show();
                         }
                     }
-                });
-
-
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this, "Register Error!" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("username", username);
+                map.put("password", password);
+                return map;
             }
-        });
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
 
-        if(result[0].equals("true")) return true;
-        /*RestTemplate restTemplate = new RestTemplate();
-        Map<String, String> map = new HashMap<>();
-        map.put("username", username.getText().toString().trim());
-        map.put("password", password.getText().toString().trim());
-        boolean ret = restTemplate.postForObject("http://coms-309-ug-09.cs.iastate.edu/add/", null, boolean.class, map);*/
-        return false;
     }
-    class MyTask extends AsyncTask<Void, Void, Void>{
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
-        }
-    }
-
 }
