b192af4e687c4d61757b57a23cf48b996eb1c7c9
William Blanchard <34970038+HalcyonGarside@users.noreply.github.com>
2020-10-07 14:52:27 -0500

    Added entities and tables to add to the database

---
+++ Backend/src/main/java/database/EventDatabase.java
@@ -0,0 +1,9 @@
+package database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventDatabase extends JpaRepository<Event, Integer> {
+
+}
