5d1ceef1ed84b13d91f855bbde38394b3785a7e0
ajgoff <ajgoff@iastate.edu>
2020-10-25 00:46:24 -0500

    added the ability to add a list to the listview in the chat.  Not connected to back end, this is just a work-around

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/ChatActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/ChatActivity.java
@@ -3,10 +3,12 @@ package com.example.loginscreen.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.springframework.web.client.RestTemplate;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.StrictMode;
+
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -24,21 +26,29 @@ import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.loginscreen.R;
 
+import android.widget.ImageButton;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private String channelID = "Channel ID";
-    private String roomName = "observable-room";
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list;
+    private ImageButton msgButton;
     private EditText editText;
     private static String API_URL = "http://coms-309-ug-09.cs.iastate.edu/database/add/";
 
@@ -49,8 +59,34 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-//        editText = (EditText) findViewById(R.id.editText);
+        listView = (ListView) findViewById(R.id.listview);
+        msgButton = (ImageButton) findViewById(R.id.msgButton);
+        msgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String logText=editText.getText().toString();
+
+                list.add(logText);
+                listView.setAdapter(arrayAdapter);
+                arrayAdapter.notifyDataSetChanged();
+                editText.getText().clear();
+            }
+        });
+
+        editText = (EditText) findViewById(R.id.chatLog);
+
+        list = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+            android.R.layout.simple_list_item_1, list);
     }
-    
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            // Send to backend
+            editText.getText().clear();
+        }
+
+    }
 
 }
