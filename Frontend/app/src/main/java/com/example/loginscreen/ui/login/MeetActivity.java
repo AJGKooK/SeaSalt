a7fa8146cb1388ed9be4332bb795ec82c0c8d429
ajgoff <ajgoff@iastate.edu>
2020-10-27 13:42:20 -0500

    testing MeetActivity stuff, forgot to switch to my branch.  Uploading this and merging over to continue work on Aaron_branch

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/MeetActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/MeetActivity.java
@@ -1,6 +1,11 @@
 package com.example.loginscreen.ui.login;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +13,28 @@ import com.example.loginscreen.R;
 
 public class MeetActivity extends AppCompatActivity {
 
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meet);
+
+        button = (Button) findViewById(R.id.chatButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openChat();
+            }
+        });
+
+
+    }
+
+    public void openChat(){
+        Uri number = Uri.parse("tel:5551234");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
 
     }
 
