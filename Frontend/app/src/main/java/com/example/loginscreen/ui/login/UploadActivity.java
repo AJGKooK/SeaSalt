17e7015a9554fab0f6cd43f9972602271c2ae524
ajgoff <ajgoff@iastate.edu>
2020-10-26 20:23:35 -0500

    UploadActivity allows file to be chosen

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/UploadActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/UploadActivity.java
@@ -4,22 +4,21 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.loginscreen.R;
 
-import java.io.File;
+import org.jetbrains.annotations.Nullable;
+
 
 public class UploadActivity extends AppCompatActivity {
 
+    private Uri selectedImage;
     private ImageButton fileFinderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
@@ -30,22 +29,28 @@ public class UploadActivity extends AppCompatActivity {
                 openFileFinder();
             }
         });
-
-
     }
 
-    public void openFileFinder(){
-//        startActivity(new Intent(Intent.ACTION_PICK)){
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.parse("file://" + "/sdcard/test.jpg"), "image/*");
-        startActivity(intent);
+    private void openFileFinder() {
+        Intent var1 = new Intent("android.intent.action.PICK");
+        var1.setType("image/*");
+        String[] mimeTypes = new String[]{"image/jpeg", "image/png"};
+        var1.putExtra("android.intent.extra.MIME_TYPES", mimeTypes);
+        this.startActivityForResult(var1, 100);
+    }
 
-
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == -1) {
+            switch(requestCode) {
+                case 100:
+                    this.selectedImage = data != null ? data.getData() : null;
+                    ImageButton fileFinderButton = this.fileFinderButton;
+                    if (fileFinderButton != null) {
+                        fileFinderButton.setImageURI(this.selectedImage);
+                    }
+            }
         }
-
-
-
+    }
 }
-
 
