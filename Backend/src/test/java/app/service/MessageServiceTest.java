5f70047b980b10b3f7b2ad90e378c647863ce1e9
William Blanchard <34970038+HalcyonGarside@users.noreply.github.com>
2020-10-27 22:38:09 -0500

    Added mockito tests for MessageService and SecurityService

---
+++ Backend/src/test/java/app/service/MessageServiceTest.java
@@ -0,0 +1,34 @@
+package app.service;
+
+import app.database.Message;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MessageServiceTest {
+
+    MessageService service;
+
+    @Mock
+    Message message;
+
+    @Test
+    public void messageSent()
+    {
+        service.addMessage(message);
+
+        List<Message> messageList = service.getAllMessages();
+
+        boolean found = false;
+        for(int i = 0; i < messageList.size(); i++)
+        {
+            if(messageList.get(i).getMsgContent().equals(message.getMsgContent()))
+                found = true;
+        }
+
+        Assert.assertTrue(found);
+    }
+}
---
+++ Backend/src/test/java/app/service/SecurityServiceTest.java
@@ -0,0 +1,60 @@
+package app.service;
+
+import app.database.Course;
+import app.database.Event;
+import app.database.Message;
+import app.database.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.Optional;
+
+public class SecurityServiceTest {
+
+    SecurityService service;
+
+    @Mock
+    User user;
+    @Mock
+    Event event;
+    @Mock
+    Message message;
+    @Mock
+    Course course;
+
+    @Test
+    public void userAuthorized()
+    {
+        service.userService.addUser(user);
+        Assert.assertTrue(service.isAuthorizedHttp(user.getUsername(), user.getPassword()).equals(user));
+    }
+
+    @Test
+    public void userInCourse()
+    {
+        user.addCourse(course);
+        service.userService.addUser(user);
+
+        Assert.assertTrue(service.isAuthorizedHttp(user.getUsername(), user.getPassword(), course).equals(course));
+    }
+
+    @Test
+    public void userInEvent()
+    {
+        event.addUser(user);
+        service.userService.addUser(user);
+
+        Assert.assertTrue(service.isAuthorizedHttp(user.getUsername(), user.getPassword(), event).equals(event));
+    }
+
+    @Test
+    public void userHasMessage()
+    {
+        user.getUserMessages().add(message);
+        service.userService.addUser(user);
+
+        Assert.assertTrue(service.isAuthorizedHttp(user.getUsername(), user.getPassword(), message).equals(message));
+    }
+}
