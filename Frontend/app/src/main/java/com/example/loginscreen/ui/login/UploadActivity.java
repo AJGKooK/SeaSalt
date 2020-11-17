aefd3da4da952df81f013b0d70390670289689d0
ajgoff <ajgoff@iastate.edu>
2020-11-16 18:46:06 -0600

    Adding dependencies to set up for file transfer from phone to server

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/UploadActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/UploadActivity.java
@@ -3,6 +3,7 @@ package com.example.loginscreen.ui.login;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -21,6 +22,7 @@ import com.example.loginscreen.R;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -105,38 +107,39 @@ public class UploadActivity extends AppCompatActivity {
      * fileUpload sends the file to the server
      */
     public void fileUpload() {
-        final String message = "this.editText.getText().toString()";
-        if (message.length() > 0) {
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            String success = response;
-                            if ((success != " ") && (UserActivity.checkUsername == UserActivity.loginUsername) && (UserActivity.checkPassword == UserActivity.loginPassword)) {
-                                Toast.makeText(UploadActivity.this, "File uploaded succesfully", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(UploadActivity.this, "File not uploaded", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(UploadActivity.this, "Upload Error!" + error.toString(), Toast.LENGTH_SHORT).show();
-                        }
-                    }) {
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> map = new HashMap<>();
-                    map.put("username", UserActivity.loginUsername);
-                    map.put("password", UserActivity.loginPassword);
-//                    map.put("/upload/user", selectedImage);
-                    return map;
-                }
-            };
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            requestQueue.add(stringRequest);
-        }
+//        final String message = "this.editText.getText().toString()";
+//
+//        if (message.length() > 0) {
+//            StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
+//                    new Response.Listener<String>() {
+//                        @Override
+//                        public void onResponse(String response) {
+//                            String success = response;
+//                            if ((success != " ") && (UserActivity.checkUsername == UserActivity.loginUsername) && (UserActivity.checkPassword == UserActivity.loginPassword)) {
+//                                Toast.makeText(UploadActivity.this, "File uploaded succesfully", Toast.LENGTH_SHORT).show();
+//                            } else {
+//                                Toast.makeText(UploadActivity.this, "File not uploaded", Toast.LENGTH_SHORT).show();
+//                            }
+//                        }
+//                    },
+//                    new Response.ErrorListener() {
+//                        @Override
+//                        public void onErrorResponse(VolleyError error) {
+//                            Toast.makeText(UploadActivity.this, "Upload Error!" + error.toString(), Toast.LENGTH_SHORT).show();
+//                        }
+//                    }) {
+//                @Override
+//                protected Map<String, String> getParams() throws AuthFailureError {
+//                    Map<String, String> map = new HashMap<>();
+//                    map.put("username", UserActivity.loginUsername);
+//                    map.put("password", UserActivity.loginPassword);
+//                    map.put("/upload/user", selectedImage.toString());
+//                    return map;
+//                }
+//            };
+//            RequestQueue requestQueue = Volley.newRequestQueue(this);
+//            requestQueue.add(stringRequest);
+//        }
 
 
     }
