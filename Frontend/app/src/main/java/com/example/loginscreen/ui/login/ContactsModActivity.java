633a90d7dfa1fdbb03182102d259bb68047498d3
ajgoff <ajgoff@iastate.edu>
2020-11-19 22:12:07 -0600

    Getting ContactsModActivity.java buttons enabled/working

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/ContactsModActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/ContactsModActivity.java
@@ -26,8 +26,9 @@ public class ContactsModActivity extends AppCompatActivity {
 
     EditText title,time,description;
     String owner = "true";
-    Button addContact;
-    Button removeContact;
+    Button addContactButton;
+    Button removeContactButton;
+    Boolean removeContact = false;
     private Map<String, String> map;
 
     /**
@@ -43,28 +44,26 @@ public class ContactsModActivity extends AppCompatActivity {
         title = findViewById(R.id.eventTitle);
         time = findViewById(R.id.timeEvent);
         description = findViewById(R.id.descriptionEvent);
-        addContact = findViewById(R.id.addContact);
-        addContact.setOnClickListener(new View.OnClickListener(){
+        addContactButton = findViewById(R.id.addContactButton);
+        addContactButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                addContact();
+                modifyContact();
             }
         });
-        removeContact = findViewById(R.id.removeContact);
-        removeContact.setOnClickListener(new View.OnClickListener(){
-            @Override
+        Button removeContactButton = (Button) findViewById(R .id.removeContactButton);
+        removeContactButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                removeContact();
+                removeContact = true;
             }
         });
-
     }
 
     /**
      * This function takes the user input from the page then sends
      * the information via hashmap to the back end using Volley Rest Api
      */
-    private void addContact() {
+    private void modifyContact() {
         final String title = this.title.getText().toString().trim();
         final String time = this.time.getText().toString().trim();
         final String description = this.description.getText().toString().trim();
@@ -101,10 +100,16 @@ public class ContactsModActivity extends AppCompatActivity {
                 Map<String, String> map = new HashMap<>();
                 map.put("username", UserActivity.loginUsername);
                 map.put("password", UserActivity.loginPassword);
-                map.put("eventName", title);
-                map.put("eventTime", time);
-                map.put("eventDesc", description);
-                map.put("owner", owner);
+                if(removeContact){
+                    
+                }
+                else{
+                    map.put("eventName", title);
+                    map.put("eventTime", time);
+                    map.put("eventDesc", description);
+                    map.put("owner", owner);
+                }
+
                 return map;
             }
         };
@@ -114,58 +119,5 @@ public class ContactsModActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
-
-    private void removeContact() {
-        final String title = this.title.getText().toString().trim();
-        final String time = this.time.getText().toString().trim();
-        final String description = this.description.getText().toString().trim();
-
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        String success = response;
-                        if ((success != (" ")) && (UserActivity.checkUsername == UserActivity.loginUsername) && (UserActivity.checkPassword == UserActivity.loginPassword)) {
-
-                            Toast.makeText(ContactsModActivity.this, "Contact removed", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(ContactsModActivity.this, "Contact Couldn't be removed, are you still logged in?", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(ContactsModActivity.this, "Contact removing Error!" + error.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-            /**
-             * holds the values the user entered when creating a new event
-             * @return the map holding the values for username, pass for login verification, event title, time, description, and owner of event
-             * @throws AuthFailureError
-             */
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> map = new HashMap<>();
-                map.put("username", UserActivity.loginUsername);
-                map.put("password", UserActivity.loginPassword);
-                map.put("eventName", title);
-                map.put("eventTime", time);
-                map.put("eventDesc", description);
-                map.put("owner", owner);
-                return map;
-            }
-        };
-        /**
-         * adding a string request to the queue
-         */
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-    }
-
-
 
 }
