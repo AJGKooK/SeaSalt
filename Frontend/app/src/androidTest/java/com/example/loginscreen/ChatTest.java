7a246d006fcc817447122cc8c9aec52b6820c38b
cjurenic <cjurenic@iastate.edu>
2020-11-20 11:36:49 -0600

    added tests for adding event and sending a message with a new username so events list visibly shows an event's ID being added to the list

--- Frontend/app/src/androidTest/java/com/example/loginscreen/ChatTest.java
+++ Frontend/app/src/androidTest/java/com/example/loginscreen/ChatTest.java
@@ -1,43 +1,18 @@
 package com.example.loginscreen;
 
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-
-import com.example.loginscreen.ui.login.EventsActivity;
-import com.example.loginscreen.ui.login.MainActivity;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import com.example.loginscreen.ui.login.LoginActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static org.hamcrest.core.StringEndsWith.endsWith;
-import static org.junit.Assert.assertEquals;
 
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import androidx.test.rule.ActivityTestRule;
-
-import com.example.loginscreen.ui.login.LoginActivity;
-import com.example.loginscreen.ui.login.MainActivity;
-import com.example.loginscreen.ui.login.UserActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -53,7 +28,7 @@ public class ChatTest {
         Intent result = new Intent();
         Instrumentation.ActivityResult activityResult = new Instrumentation.ActivityResult(Activity.RESULT_OK, result);
 
-        String user = "cjurenic";
+        String user = "cjurenic1";
         String pass = "123123";
         String message = "This is my chat mockito test";
 
