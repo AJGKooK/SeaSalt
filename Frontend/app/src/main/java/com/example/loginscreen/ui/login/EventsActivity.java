733eb0b567b862e1abeb67c4f77cdf1f6247027e
ajgoff <ajgoff@iastate.edu>
2020-11-22 22:04:27 -0600

    Fixed create event

--- Frontend/app/src/main/java/com/example/loginscreen/ui/login/EventsActivity.java
+++ Frontend/app/src/main/java/com/example/loginscreen/ui/login/EventsActivity.java
@@ -2,6 +2,7 @@ package com.example.loginscreen.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,8 +19,11 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.loginscreen.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * The Event activity for Sea Salt
@@ -33,6 +37,7 @@ public class EventsActivity extends AppCompatActivity {
     private static String API_URL = "http://coms-309-ug-09.cs.iastate.edu/event/";
     EditText title,time,description;
     String owner = "true";
+    private final String events = "";
     Button addEvent;
     private Map<String, String> map;
     private int j;
@@ -181,25 +186,64 @@ public class EventsActivity extends AppCompatActivity {
 
     }
     public void getEvents(){
+        final ArrayList<String> contacts = new ArrayList<>();
         RequestQueue queue = Volley.newRequestQueue(EventsActivity.this);
         String url = "http://coms-309-ug-09.cs.iastate.edu/user/events/involved?username=" + UserActivity.loginUsername +"&password=" + UserActivity.loginPassword;
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
             @Override
-            public void onResponse(String response){
+            public void onResponse(String response) {
+                EventsMainActivity.textView.setText("Upcoming events:\n");
+                if (response.length() == 2) {
+                    EventsMainActivity.textView.setText("You have no events");
+                } else {
+                    response = response.substring(1, response.length() - 1);
+                    String[] usernames = response.split(",");
+                    for (String bubbles : usernames) {
+                        bubbles = bubbles.replace("\"", "");
+                        RequestQueue queue = Volley.newRequestQueue(EventsActivity.this);
+                        String url = "http://coms-309-ug-09.cs.iastate.edu/event/info?username=" + UserActivity.loginUsername + "&password=" + UserActivity.loginPassword + "&id=" + bubbles;
+                        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                int i = 0;
+                                Pattern p = Pattern.compile("\"([^\"]*)\"");
+                                Matcher m = p.matcher(response);
+                                while (m.find()) {
+                                    if (i == 0)
+                                        EventsMainActivity.textView.append("\n");
+                                    if (i%2 == 0)
+                                        EventsMainActivity.textView.append(m.group(1) + ": ");
+                                    else
+                                        EventsMainActivity.textView.append(m.group(1) + "\n");
+                                    i++;
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                EventsMainActivity.textView.setText("Events Failed to display: ");
+                                Log.i("Event list", "Event adding failed");
+                            }
+                        }) {
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> map = new HashMap<>();
+                                map.put("username", UserActivity.loginUsername);
+                                map.put("password", UserActivity.loginPassword);
+                                map.put("id", events);
+                                return map;
+                            }
+                        };
+                        queue.add(stringRequest);
 
-                response = response.replace("[", "");
-                response = response.replace("]", "");
-                response = response.replaceAll(",", "");
-                for(int i = 0; i <= response.length()-1; i++){
-                    final char event = response.charAt(i);
-                    final String events = Character.toString(event);
-                    EventsMainActivity.textView.append("\n" + events);
+                    }
                 }
             }
-
         }, new Response.ErrorListener(){
             @Override
             public void onErrorResponse(VolleyError error){
+//                ContactsActivity.textView.setText("Events Failed to display");
+                Log.i("Event list", "Event adding failed");
             }
         }){
             @Override
@@ -207,12 +251,11 @@ public class EventsActivity extends AppCompatActivity {
                 Map<String, String> map = new HashMap<>();
                 map.put("username", UserActivity.loginUsername);
                 map.put("password", UserActivity.loginPassword);
-
                 return map;
             }
         };
-
         queue.add(stringRequest);
+
 
     }
 }
